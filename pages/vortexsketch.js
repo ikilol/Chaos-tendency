fe0443123028f967b83243a9bd092c0ea53b175e
ikilol <87668373+ikilol@users.noreply.github.com>
2022-01-29 16:23:26 +0100

    pusho 3

---
+++ pages/vortexsketch.js
@@ -0,0 +1,19 @@
+let index = 0;
+let lastIndex = 0;
+let ele;
+let socket;
+
+function setup() {
+
+  ele = createAudio('Audio/mySound.wav');
+  ele.volume(0.1);
+  ele.loop();
+  ele.autoplay(true);
+  
+}
+
+
+if (index != lastIndex) {
+
+
+}
