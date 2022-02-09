3554395c78ec7298b98969b28c2b4b84aba0c4e5
B0tta <luca.bottani00@gmail.com>
2022-02-10 00:12:33 +0100

    blob audio

--- recordsketch.js
+++ recordsketch.js
@@ -5,13 +5,14 @@ let stops = "stop";
 let record = "record";
 let ok1 = false;
 
+let fft;
+
 let kMax; // maximal value for the parameter "k" of the blobs
 let step = 0.01; // difference in time between two consecutive blobs
 let n = 100; // total number of blobs
 let radius = 0; // radius of the base circle
 let inter = 0.05; // difference of base radii of two consecutive blobs
 let maxNoise = 500; // maximal value for the parameter "noisiness" for the blobs
-
 
 let counter;
 
@@ -20,11 +21,11 @@ function setup() {
 
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB, 1);
-	angleMode(DEGREES);
+  angleMode(DEGREES);
   noFill();
-	//noLoop();
-	kMax = random(0.6, 1.0);
-	noStroke();
+  //noLoop();
+  kMax = random(0.6, 1.0);
+  noStroke();
 
   // counter per numero di audio registrati
   counter = getItem("counter");
@@ -44,71 +45,65 @@ function setup() {
 
   soundFile = new p5.SoundFile();
 
+  fft = new p5.FFT();
+  fft.setInput(mic);
 }
 
-  userStartAudio();
-
-
-
+userStartAudio();
 
 function draw() {
   background(0);
 
-  if (state==0) {
-  
+  if (state == 0) {
     textFont("Roboto mono");
     textAlign(CENTER);
     record = createElement("h1", "record");
-    record.style('color', '#ffffff');
-    record.style('font-family', 'Roboto mono');
-    record.style('font-size', 'windowHeight');
+    record.style("color", "#ffffff");
+    record.style("font-family", "Roboto mono");
+    record.style("font-size", "windowHeight");
     record.position(windowWidth / 2, windowHeight / 1.05);
+  }
 
-  } 
-
-  if (state==1) {
+  if (state == 1) {
     recorder.record(soundFile);
-  
+
     textFont("Roboto mono");
     textAlign(CENTER);
     textSize(windowHeight / 35);
     noStroke();
     fill(255);
-    
-    text(stops, windowWidth / 2, windowHeight / 1.05);
-  } 
 
-  if (state==2){
+    text(stops, windowWidth / 2, windowHeight / 1.05);
+  }
+
+  if (state == 2) {
     recorder.stop();
     soundFile.play();
   }
 
-  if (state==3){
-
+  if (state == 3) {
     counter++;
     storeItem("counter", counter);
     save(soundFile, "mySound.wav");
     state = 0;
   }
-  
 
-    let t = frameCount/100;
+  let t = frameCount / 100;
   for (let i = n; i > 0; i--) {
-		let alpha = 1 - (i / n);
-		fill((alpha/5 + 0.90)%9, 1, 2, alpha);
-		let size = radius + i * inter;
-		let k = kMax * sqrt(i/n);
-		let noisiness = maxNoise * (i / n);
-    blob(size, width/2, height/2, k, t - i * step, noisiness);
+    let alpha = 1 - i / n;
+    fill((alpha / 5 + 0.9) % 9, 1, 2, alpha);
+    let size = radius + i * inter;
+    let k = kMax * sqrt(i / n);
+    let noisiness = maxNoise * (i / n);
+    blob(size, width / 2, height / 2, k, t - i * step, noisiness);
   }
-
 
   textFont("Roboto mono");
   textAlign(CENTER);
   textSize(windowHeight / 35);
   noStroke();
   fill(255);
-  let sendg = text("//chaos tendency", windowWidth / 12, windowHeight/ 15);
+  let sendg = text("//chaos tendency", windowWidth / 12, windowHeight / 15);
 
   textFont("Roboto mono");
   textAlign(LEFT);
@@ -126,18 +121,22 @@ function draw() {
 }
 
 function blob(size, xCenter, yCenter, k, t, noisiness) {
+  let spectrum = fft.analyze();
   beginShape();
-	let angleStep = 360 / 10;
+  let angleStep = 360 / 10;
   for (let theta = 0; theta <= 360 + 2 * angleStep; theta += angleStep) {
     let r1, r2;
-		r1 = cos(theta)+1;
-		r2 = sin(theta)+1; // +1 because it has to be positive for the function noise
-    let r = size + noise(k * r1,  k * r2, t) * noisiness;
+    r1 = cos(theta) + 1;
+    r2 = sin(theta) + 1; // +1 because it has to be positive for the function noise
+    let r =
+      size +
+      map(spectrum[theta], 0, 255, 100, 0) +
+      (noise(k * r1, k * r2, t) * noisiness) / 3;
+    //noise(k * r1, k * r2, t) * noisiness;
+    //map(spectrum[theta], 0, 255, 200, 0);
     let x = xCenter + r * cos(theta);
     let y = yCenter + r * sin(theta);
     curveVertex(x, y);
   }
   endShape();
 }
-
-
