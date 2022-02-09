0d327784b5652eab90b387d6537723bedda3f2fb
B0tta <luca.bottani00@gmail.com>
2022-02-09 14:45:57 +0100

    PUSH

--- recordsketch.js
+++ recordsketch.js
@@ -1,13 +1,21 @@
-
 let mic, recorder, soundFile;
 let state = 0;
 let listen;
+
+let counter;
 
 function setup() {
   let cnv = createCanvas(windowWidth - 20, 550);
   cnv.mousePressed(canvasPressed);
   background(220);
   textAlign(CENTER, CENTER);
+
+  counter = getItem("counter");
+  if (counter === null) {
+    counter = "";
+  }
+
+  console.log(counter);
 
   // create an audio in
   mic = new p5.AudioIn();
@@ -25,7 +33,7 @@ function setup() {
   // playback & save the recording
   soundFile = new p5.SoundFile();
 
-  text('tap to record', width/2, height/2);
+  text("tap to record", width / 2, height / 2);
 }
 
 function canvasPressed() {
@@ -34,76 +42,70 @@ function canvasPressed() {
 
   // make sure user enabled the mic
   if (state === 0 && mic.enabled) {
-
     // record to our p5.SoundFile
     recorder.record(soundFile);
 
-    background(255,0,0);
-    text('Recording!', width/2, height/2);
+    background(255, 0, 0);
+    text("Recording!", width / 2, height / 2);
     state++;
-  }
-  else if (state === 1) {
-    background(0,255,0);
+  } else if (state === 1) {
+    background(0, 255, 0);
 
     // stop recorder and
     // send result to soundFile
     recorder.stop();
 
-    text('Done! Tap to play and download', width/2, height/2, width - 20);
+    text("Done! Tap to play and download", width / 2, height / 2, width - 20);
     state++;
-  }
-
-  else if (state === 2) {
+  } else if (state === 2) {
+    counter++;
+    storeItem("counter", counter);
     soundFile.play(); // play the result!
-    save(soundFile, 'mySound.wav');
-    state++;
+    save(soundFile, "mySound.wav");
+    state = 0;
   }
 }
 
-function draw(){
-
+function draw() {
   background(220);
 
   textFont("Quantico");
   textAlign(LEFT);
-  textSize(windowHeight/30);
+  textSize(windowHeight / 30);
   noStroke();
   fill(255);
-  let deletex = text("DELETE", windowHeight/25, windowHeight/1.4);
+  let deletex = text("DELETE", windowHeight / 25, windowHeight / 1.4);
 
   textFont("Quantico");
   textAlign(LEFT);
-  textSize(windowHeight/30);
+  textSize(windowHeight / 30);
   noStroke();
   fill(255);
-  let listen = text("LISTEN", windowHeight/5, windowHeight/1.4);
+  let listen = text("LISTEN", windowHeight / 5, windowHeight / 1.4);
 
   textFont("Quantico");
   textAlign(CENTER);
-  textSize(windowHeight/30);
+  textSize(windowHeight / 30);
   noStroke();
   fill(255);
-  let record = text("RECORD", windowWidth/2, windowHeight/1.4);
+  let record = text("RECORD", windowWidth / 2, windowHeight / 1.4);
 
   textFont("Quantico");
   textAlign(CENTER);
-  textSize(windowHeight/30);
+  textSize(windowHeight / 30);
   noStroke();
   fill(255);
-  let play = text("STOP", windowWidth/2, windowHeight/1.4);
+  let play = text("STOP", windowWidth / 2, windowHeight / 1.4);
 
   textFont("Quantico");
   textAlign(CENTER);
-  textSize(windowHeight/30);
+  textSize(windowHeight / 30);
   noStroke();
   fill(255);
-  let send = text("SEND", windowWidth/0.9, windowHeight/1.4);
-
-
+  let send = text("SEND", windowWidth / 0.9, windowHeight / 1.4);
 }
 
-function changecolor(){
+function changecolor() {
   fill(0);
-  let listen = text("LISTEN", windowHeight/5, windowHeight/1.4);
+  let listen = text("LISTEN", windowHeight / 5, windowHeight / 1.4);
 }
-
--- vortexsketch.js
+++ vortexsketch.js
@@ -1,22 +1,21 @@
 let p;
 let ele;
 let particles = [];
-const num = 4000;
-const noiseScale = 0.01/2;
-
-
-
+let num;
+const noiseScale = 0.01 / 2;
+let numeroaudio;
 
 function setup() {
-
   createCanvas(displayWidth, displayHeight);
-  for(let i = 0; i < num; i ++) {
+
+  num = getItem("counter");
+  console.log(num);
+
+  for (let i = 0; i < num; i++) {
     particles.push(createVector(random(width), random(height)));
   }
-  
+
   stroke(96, 225, 224);
-
-
 
   var num1 = int(random(1, 10));
   console.log(num1);
@@ -105,27 +104,15 @@ function setup() {
 function draw() {
   background(0, 10);
   stroke(96, 225, 224);
-  for(let i = 0; i < num; i ++) {
-    let p = particles[i];
-    point(p.x, p.y);
-    let n = noise(p.x * noiseScale, p.y * noiseScale, frameCount * noiseScale);
-    let a = TAU * n;
-    p.x += cos(a);
-    p.y += sin(a);
-    if(!onScreen(p)) {
-      p.x = random(width);
-      p.y = random(height);
-    }
-  }
-  
-    
+
+  flock();
+
   textFont("Quantico");
   textAlign(LEFT);
   textSize(30);
   noStroke();
   fill(255);
   text("THE SOUND\nOF CHAOS", 20, 50);
-
 }
 
 function mouseReleased() {
@@ -140,5 +127,17 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-
-  
+function flock() {
+  for (let i = 0; i < num; i++) {
+    let p = particles[i];
+    point(p.x, p.y);
+    let n = noise(p.x * noiseScale, p.y * noiseScale, frameCount * noiseScale);
+    let a = TAU * n;
+    p.x += cos(a);
+    p.y += sin(a);
+    if (!onScreen(p)) {
+      p.x = random(width);
+      p.y = random(height);
+    }
+  }
+}
