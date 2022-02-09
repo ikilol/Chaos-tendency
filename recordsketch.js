d2c4a50c489981b24652bf37f408e4257b67c5ef
B0tta <luca.bottani00@gmail.com>
2022-02-09 15:39:36 +0100

    push controllo

--- recordsketch.js
+++ recordsketch.js
@@ -10,6 +10,7 @@ function setup() {
   background(220);
   textAlign(CENTER, CENTER);
 
+  // counter per numero di audio registrati
   counter = getItem("counter");
   if (counter === null) {
     counter = "";
@@ -17,50 +18,53 @@ function setup() {
 
   console.log(counter);
 
-  // create an audio in
+  // crea audio
   mic = new p5.AudioIn();
 
-  // prompts user to enable their browser mic
+  // abilita microfono browser
   mic.start();
 
-  // create a sound recorder
+  // crea il sound recorder
   recorder = new p5.SoundRecorder();
 
-  // connect the mic to the recorder
+  // connette il microfono al recorder
   recorder.setInput(mic);
 
-  // this sound file will be used to
-  // playback & save the recording
+  // salvataggio registrazione
   soundFile = new p5.SoundFile();
 
   text("tap to record", width / 2, height / 2);
 }
 
 function canvasPressed() {
-  // ensure audio is enabled
+  // assicura che l'audio è abilitato
   userStartAudio();
 
-  // make sure user enabled the mic
+  // assicura che microfono è abilitato
   if (state === 0 && mic.enabled) {
-    // record to our p5.SoundFile
+    // registraa sul file salvato
     recorder.record(soundFile);
 
     background(255, 0, 0);
-    text("Recording!", width / 2, height / 2);
+    text("Recording...", width / 2, height / 2);
     state++;
   } else if (state === 1) {
     background(0, 255, 0);
 
-    // stop recorder and
-    // send result to soundFile
+    // stop registrazione e salva file
     recorder.stop();
 
-    text("Done! Tap to play and download", width / 2, height / 2, width - 20);
+    text(
+      "Done! Tap to play and upload it to the vortex",
+      width / 2,
+      height / 2,
+      width - 20
+    );
     state++;
   } else if (state === 2) {
     counter++;
     storeItem("counter", counter);
-    soundFile.play(); // play the result!
+    soundFile.play(); // riproduce il risultato
     save(soundFile, "mySound.wav");
     state = 0;
   }
